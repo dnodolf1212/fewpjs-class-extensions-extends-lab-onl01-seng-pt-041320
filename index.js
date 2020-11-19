44dd94c451318d878efd2c6056a58f973ff40902
dnodolf1212 <dnodolf@gmail.com>
2020-11-18 21:06:58 -0800

    commit tree 10e25815af86e5543b9405e02e5d056c9176f8ce

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+class Polygon {
+  constructor(nums){
+    this.nums = nums;
+  }
+
+  get countSides(){
+   return this.nums.length;
+  }
+
+  get perimeter(){
+    return this.nums.reduce(acc, 0)
+  }
+
+}
